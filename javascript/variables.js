5907dcb9bf7c6c737f02122a78584741e95fe855
KevinOrosco <kevinorosco411@gmail.com>
2024-04-22 17:03:43 -0300

    agregue las carpetas de ejercicios y la de pokemones

---
+++ javascript/variables.js
@@ -0,0 +1,17 @@
+var numero = 1; //asignamos un valor de tipo int a una variable
+var palabra = 'a'; //asignamos un valor de tipo string a una variable
+
+//definimos variables con la palabra reservada let para evitar el llamado de la misma en cualquier parte del codigo
+let numero2 = 2;
+let palabra2 = "palabra";
+
+//definimos constantes con la palanra reservada const
+const numconstante = 4; //siempre que sepan que no van a mutar o modificar un valor con const
+
+//Ejemplo:
+
+if(numero > 0){
+    let numero3 = 3
+}
+
+console.log("numero",numero2) //mostrar un valor por pantalla
