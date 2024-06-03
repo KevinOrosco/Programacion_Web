a7f14e7f0f1f5b77c5a9f404d1bee3ff6e4c8d05
KevinOrosco <kevinorosco411@mail.com>
2024-06-03 17:01:44 -0300

    Agregue el archivo react con el contenido de lo visto el 3/06 en clase

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import {MiPrimerComponente} from "./components/mi_primer_componente/miPrimerComponente";
+
+function App() {
+  
+  return  (<> 
+  <MiPrimerComponente name={"pipe"} lastName={"atom"}/>
+  </>);
+}
+
+export default App;
---
+++ react/vite-project/src/components/mi_primer_componente/miPrimerComponente.jsx
@@ -0,0 +1,7 @@
+export function MiPrimerComponente({name, lastName}) {
+    return (<> 
+    Hola mundo soy {name} {lastName}
+    <img src="/imgs/pe.jpeg" alt="mimimiimimimimmiiimimimi" />
+    </>
+    );
+}
